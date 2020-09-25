f64e29f78a1c6fe483bcbf53d09c8a891a642b1e
Oskari-Juvakka <oskari.juvakka@gmail.com>
2020-09-25 16:41:08 +0300

    Layout -pohja lisätty ja securitykonfiguraatiot eri profiileille luotu

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/etusivu")
+    @GetMapping("/welcome")
     public String helloWorld(Model model) {
         return "index";
     }
 
     @GetMapping("*")
     public String redirectToIndex() {
-        return "redirect:/etusivu";
+        return "redirect:/welcome";
     }
 }
--- src/main/java/projekti/DevelopmentSecurityConfiguration.java
+++ src/main/java/projekti/DevelopmentSecurityConfiguration.java
@@ -1,15 +1,45 @@
 package projekti;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class DevelopmentSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //@Override
+    //public void configure(WebSecurity sec) throws Exception {
+    // Pyyntöjä ei tarkasteta
+    //  sec.ignoring().antMatchers("/**");}
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
     @Override
-    public void configure(WebSecurity sec) throws Exception {
-        // Pyyntöjä ei tarkasteta
-        sec.ignoring().antMatchers("/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/signup", "/signup/**").permitAll()
+                .antMatchers("/welcome", "/welcome/**").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
