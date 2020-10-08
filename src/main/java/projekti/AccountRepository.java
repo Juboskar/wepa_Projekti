4b2bea99156d41c7b0700c1d857863e0d769f5a2
Oskari-Juvakka <oskari.juvakka@gmail.com>
2020-10-08 22:03:38 +0300

    pieniä muutoksia

--- src/main/java/projekti/AccountRepository.java
+++ src/main/java/projekti/AccountRepository.java
@@ -6,10 +6,8 @@
 package projekti;
 
 
-import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Transactional
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
     Account findByUserpath(String userpath);
